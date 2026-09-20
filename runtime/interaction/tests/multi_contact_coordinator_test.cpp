1e84fbd65b174e4c78e021d898a97842f229a984
Chad <luckkievan@gmail.com>
2026-09-21 06:51:34 +0800

    test: close corrective multi-contact evidence bundle

--- runtime/interaction/tests/multi_contact_coordinator_test.cpp
+++ runtime/interaction/tests/multi_contact_coordinator_test.cpp
@@ -43,12 +43,24 @@ int main() {
   assert(multiInk.update(sample(b, PointerPhase::kDown, 20, 0, 2)) == ContactDisposition::kInk);
   assert(!multiInk.viewportClaimed());
   assert(multiInk.activeCount() == 2);
+  const PointerKey c{1, 3, 1};
+  const PointerKey d{1, 4, 1};
+  assert(multiInk.update(sample(c, PointerPhase::kDown, 40, 0, 3)) == ContactDisposition::kInk);
+  assert(multiInk.update(sample(d, PointerPhase::kDown, 60, 0, 4)) == ContactDisposition::kInk);
+  assert(multiInk.activeCount() == 4);
+  assert(multiInk.update(sample(c, PointerPhase::kCancel, 40, 0, 5)) == ContactDisposition::kInk);
+  assert(multiInk.activeCount() == 3);
 
   MultiContactCoordinator priority(MultiContactPolicy::kGesturePriority);
   assert(priority.update(sample(a, PointerPhase::kDown, 0, 0, 1)) == ContactDisposition::kPending);
   assert(priority.update(sample(a, PointerPhase::kMove, 20, 0, 2)) == ContactDisposition::kInk);
   assert(priority.update(sample(b, PointerPhase::kDown, 0, 0, 3)) == ContactDisposition::kIgnored);
   assert(priority.update(sample(a, PointerPhase::kMove, 30, 0, 4)) == ContactDisposition::kInk);
+
+  MultiContactCoordinator spatial;
+  assert(spatial.update(sample({3, 1, 1}, PointerPhase::kDown, 0, 0, 1)) == ContactDisposition::kPending);
+  assert(spatial.update(sample({3, 1, 1}, PointerPhase::kMove, 3, 4, 2)) == ContactDisposition::kPending);
+  assert(spatial.update(sample({3, 1, 1}, PointerPhase::kMove, 8, 0, 3)) == ContactDisposition::kInk);
 
   MultiContactCoordinator predicted;
   auto predictedDown = sample({2, 1, 1}, PointerPhase::kDown, 0, 0, 1);
